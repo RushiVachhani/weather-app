586c475b8779522254c14e9f4f998e2815a3837e
RushiVachhani <vachhanirushi30@gmail.com>
2021-02-06 19:52:57 +0530

    completed function for updating html

--- app_modified.js
+++ app_modified.js
@@ -1,6 +1,6 @@
 //extracting the elements from html
 
-const background_img = document.querySelector('.background-image');
+const background_img = document.querySelector('.background-img');
 const latitude = document.querySelectorAll('.coordinate-value')[0];
 const longitude = document.querySelectorAll('.coordinate-value')[1];
 const city = document.querySelector('.city > h2');
@@ -52,13 +52,59 @@ async function getWeatherData(lat, long) {
 
 //function for updating the html based on data received
 function updateHtml(data) {
+    background_img.setAttribute('src', getBackgroundImgUrl());
     latitude.textContent = parseFloat(data.coord.lat).toFixed(2);
     longitude.textContent = parseFloat(data.coord.lon).toFixed(2);
     tempvalue.textContent = data.main.temp;
     minTemp.textContent = data.main.temp_min;
     maxTemp.textContent = data.main.temp_max;
     feelsLike.textContent = data.main.feels_like;
-    
+    pressure.textContent = `Pressure : ${data.main.pressure} hPa`;
+    humidity.textContent = `Humidity : ${data.main.humidity} %`;
+    visibility.textContent = `Visibility : ${(parseFloat(data.visibility)/1000).toFixed(2)} km`;
+    wind.textContent = `Wind : ${data.wind.speed} m/s ${getWindDirection(data.wind.deg)}`;
+    tempDescription.textContent = data.weather[0].description;
+    country.textContent = data.sys.country;
+    city.textContent = `${data.name},`;
+    sunriseTime.textContent = convertUnixTime(data.sys.sunrise);
+    sunsetTime.textContent = convertUnixTime(data.sys.sunset);
+    weatherIcon.setAttribute('src', getWeatherIconUrl(data.weather[0].icon));
+    countryFlag.setAttribute('src', getCountryFlagIconUrl(data.sys.country));
+}
+
+//function to determine wind direction from degree
+function getWindDirection(deg) {
+    let direction = ['N', 'NW', 'W', 'SW', 'S', 'SE', 'E', 'NE'];
+    return direction[(Math.round((deg/45)%8))];
+}
+
+//function to convert Unix timestamp to local time string
+function convertUnixTime(time) {
+    const date = new Date( parseInt(time)*1000);
+    return date.toLocaleTimeString();
+}
+
+//function to get the weather icon url
+function getWeatherIconUrl(iconId) {
+    return `http://openweathermap.org/img/wn/${iconId}@4x.png`;
+}
+
+//function to get the country flag icon url
+function getCountryFlagIconUrl(countryCode) {
+    let flagStyle = "flat";
+    let flagSize = "48";
+    return `https://www.countryflags.io/${countryCode}/${flagStyle}/${flagSize}.png`;
+}
+
+//function to determine the background image
+function getBackgroundImgUrl() {
+    let date = new Date();
+    let imgUrl = ''
+    //date.toTimeString();
+    if(date.getHours()>= 06 && date.getHours()<= 17) imgUrl = "./assets/morning.jpg";
+    else if(date.getHours()>= 17 && date.getHours()<= 20) imgUrl = "./assets/evening.jpg";
+    else imgUrl = "./assets/night.png";
+    return imgUrl;
 }
 
 //console.log(pressure.textContent.split(" ")[2].split("hPa")[0])
